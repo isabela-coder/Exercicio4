e588adf0f83c7dee78a57bbb2366fb69048dff98
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:13:52 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio4/Exercicio4.java
@@ -0,0 +1,27 @@
+
+package exercicio4;
+
+import java.util.Scanner;
+
+
+public class Exercicio4 {
+
+    
+    public static void main(String[] args) {
+       
+         Scanner numero= new Scanner(System.in);
+        
+           int entrada;
+          double resultado;
+      
+      System.out.println("Digite o numero inteiro:");
+      entrada= numero.nextInt();
+     
+      resultado = entrada*entrada;
+      System.out.println(resultado);
+    
+     
+        
+    }
+    
+}
